436fce1b2866672af9968ea13dd3b76728141af1
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-19 17:34:03 +0100

    create controller for email verification and user account status change

--- src/controllers/UpdatePass.ts
+++ src/controllers/UpdatePass.ts
@@ -6,40 +6,40 @@ import {checkValidPassword, hashSync} from "../libs/libs.js";
 import user from "../models/user.js";
 
 export default async function UpdatePass (req:Request, res:Response, next:NextFunction) {
-            // All fields are required
-        let required_fields = ["password", "password_confirmation"];
-        for (let i = 0; i <required_fields.length; i++) {
-            if(!req.body[required_fields[i]]){
-                return next(new Error(`${required_fields[i]} is required`));
-            }
+        // All fields are required
+    let required_fields = ["password", "password_confirmation"];
+    for (let i = 0; i <required_fields.length; i++) {
+        if(!req.body[required_fields[i]]){
+            return next(new Error(`${required_fields[i]} is required`));
         }
-        // user must be authenticated before he perform this action
-        if (!checkUserAuthentication(req, res, next)){
-            return AuthenticationRequired(req, res , next);
-        }
-        // We hope to have the password, password_confirmation fields in the body request
-        let [pass, confirm_pass] = [req.body.password, req.body.password_confirmation];
-        // the two password (password and password confirmation) must be equal and valid
-        if (
-        !checkValidPassword(pass) ||
-        !checkValidPassword(confirm_pass) ||
-        (pass !==  confirm_pass) ||( pass === "") || (confirm_pass === "") )
-        {
-            return InvalidCredentials(res);
-        }else if(pass === confirm_pass) {
-            let replacement = await user.findOneAndUpdate({
-                token : req.cookies[config.user_token]
-            }, {
-                password : hashSync(confirm_pass)
-            }, {
-                new : true
-            });
-            if (replacement){
-                return Succes(res,replacement);
-            }else{
-                return UnknowError(res);
-            }
+    }
+    // user must be authenticated before he perform this action
+    if (!checkUserAuthentication(req, res, next)){
+        return AuthenticationRequired(req, res , next);
+    }
+    // We hope to have the password, password_confirmation fields in the body request
+    let [pass, confirm_pass] = [req.body.password, req.body.password_confirmation];
+    // the two password (password and password confirmation) must be equal and valid
+    if (
+    !checkValidPassword(pass) ||
+    !checkValidPassword(confirm_pass) ||
+    (pass !==  confirm_pass) ||( pass === "") || (confirm_pass === "") )
+    {
+        return InvalidCredentials(res);
+    }else if(pass === confirm_pass) {
+        let replacement = await user.findOneAndUpdate({
+            token : req.cookies[config.user_token]
+        }, {
+            password : hashSync(confirm_pass)
+        }, {
+            new : true
+        });
+        if (replacement){
+            return Succes(res,replacement);
         }else{
             return UnknowError(res);
         }
+    }else{
+        return UnknowError(res);
+    }
 }
---
+++ src/controllers/ValidateEmailAddress.ts
@@ -0,0 +1,22 @@
+/**
+ * @author [Judarist Fullstack]
+ * @email [judearist@mail.com]
+ * @create date 2022-02-19 17:09:14
+ * @modify date 2022-02-19 17:09:14
+ * @desc [ This function is a controller that respond to the route /account/activate?token=...
+ * and update the account status in db if the account has not yet been verified; Id the account
+ * is already verfied, send the appriate response to the user ]
+ */
+
+
+import { NextFunction } from "express";
+
+/**
+ * TODO: Implement this function
+ * @param req 
+ * @param res 
+ * @param next 
+ */
+export default function (req:Express.Request, res:Express.Response, next:NextFunction) {
+
+}
