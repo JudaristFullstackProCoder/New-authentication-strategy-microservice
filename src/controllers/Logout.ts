a121ef097a75f089413efe50cd6dd5a1af7b597a
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-21 19:39:38 +0100

    Add author info

--- src/controllers/Logout.ts
+++ src/controllers/Logout.ts
@@ -1,3 +1,11 @@
+/**
+ * @author [Judarist Fullstack]
+ * @email [judearist@mail.com]
+ * @create date 2022-02-21 18:37:29
+ * @modify date 2022-02-21 18:37:29
+ * @desc [Logout controller]
+ */
+
 import { NextFunction, Request, Response } from "express";
 import {Logout} from "../middlewares/Responses.js";
 
