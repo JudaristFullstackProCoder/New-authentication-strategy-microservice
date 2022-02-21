97fd46265e48960814e0118728471b14f842a023
judarist Fullstack <jude22fullstack@gmail.com>
2022-02-21 19:37:34 +0100

    passwordreset model

---
+++ src/models/passwordReset.ts
@@ -0,0 +1,34 @@
+/**
+ * @author [Judarist Fullstack]
+ * @email [judearist@mail.com]
+ * @create date 2022-02-21 18:45:25
+ * @modify date 2022-02-21 18:45:25
+ * @desc [Model for password reset table]
+ */
+
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const passResetSchema = new Schema({
+    email: {
+        type: Schema.Types.String,
+        required: true
+    },
+    code: {
+        type: Schema.Types.String,
+        required: true
+    },
+    status : {
+        type: Schema.Types.String,
+        required: true,
+        enum: ["started", "pending", "done"],
+        default: "started"
+        /**
+         * started : the user made a request to reset his password and a code was sent to him by email
+         * pending : the user to enter the corresponding code allowing him to reset his password
+         * done : the user to reset his password after entering the code
+         */
+    }
+});
+
+export default mongoose.model("passwordreset", passResetSchema);
