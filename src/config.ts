aa268a5ba26bb4248f5a90c5489892a51abfab6a
Judarist Fullstack Procoder <judearist@gmail.com>
2022-02-27 22:18:47 +0100

    remove useless file

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-
-export default {
-    port : 4422,
-    mongodb:`mongodb://localhost:27017/authentication`,
-    salt : `$2a$10$3dInsPSO2UVHx4bhMbNM5ehTU.oz3NUlpbZftdztTT26W1TdowDD2`,
-    user_token : "user_token"
-};
