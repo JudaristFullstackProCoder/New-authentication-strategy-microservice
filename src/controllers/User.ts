7ca6d22c5437116d123796de1eb8295ac2d18e5a
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-01 16:59:24 +0100

    create User controller

---
+++ src/controllers/User.ts
@@ -0,0 +1,22 @@
+/**
+ * @author [Judarist Fullstack]
+ * @email [judearist@mail.com]
+ * @create date 2022-03-01 16:45:16
+ * @modify date 2022-03-01 16:45:16
+ * @desc [User controller]
+ */
+
+import { NextFunction, Request, Response } from "express"
+
+
+export const getUser = async function (req:Request, res:Response, next:NextFunction) {
+
+}
+
+export const DeleteUser = async function (req:Request, res:Response, next:NextFunction) {
+
+}
+
+export const UpdateUser = async function (req:Request, res:Response, next:NextFunction) {
+
+}
