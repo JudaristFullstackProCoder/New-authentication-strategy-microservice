5557a2fbfe5c8f4d4c15ae6d135ff3e592c74e4a
Judarist Fullstack Procoder <jude22fullstack@gmail.com>
2022-03-01 17:32:25 +0100

    Implement all controller function

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -6,17 +6,44 @@
  * @desc [User controller]
  */
 
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
+import User from "../models/user.js";
 
 
 export const getUser = async function (req:Request, res:Response, next:NextFunction) {
-
+    let id = req.body.id;
+    User.findById(id, {}, {}, function (err, data) {
+        if (err) return next(err);
+        return res.json({
+            success: true,
+            data: data
+        });
+    });
 }
 
 export const DeleteUser = async function (req:Request, res:Response, next:NextFunction) {
-
+    let id = req.body.id;
+    User.findOneAndDelete({
+        _id: id
+    }, {}, function (err, data) {
+        if (err) return next(err);
+        return res.json({
+            success: true,
+            data: data
+        })
+    });
 }
 
 export const UpdateUser = async function (req:Request, res:Response, next:NextFunction) {
-
+    let id = req.body.id;
+    delete req.body.id;
+    User.findByIdAndUpdate(id, {
+        ...req.body
+    }, {new: true}, function (err, data) {
+        if (err) return next(err);
+        return res.json({
+            success: true,
+            data: data
+        });
+    });
 }
